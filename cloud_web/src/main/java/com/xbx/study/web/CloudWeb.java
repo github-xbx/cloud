86aaf9f769140d20b90f324bc906468353adcfea
xing <3154401838@qq.com>
2025-06-22 10:01:29 +0800

    web project inti

---
+++ cloud_web/src/main/java/com/xbx/study/web/CloudWeb.java
@@ -0,0 +1,11 @@
+package com.xbx.study.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudWeb {
+    public static void main(String[] args) {
+        SpringApplication.run(CloudWeb.class,args);
+    }
+}
---
+++ cloud_web/src/main/java/com/xbx/study/web/config/BeanConfiguration.java
@@ -0,0 +1,22 @@
+package com.xbx.study.web.config;
+
+import com.alibaba.druid.pool.DruidDataSource;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class BeanConfiguration {
+
+    /**
+     * 需要配置bean 否则会找不到数据库配置信息
+     * @return
+     */
+    //@ConfigurationProperties(prefix = "spring.datasource")
+    @Bean
+    public DataSource druid() {
+        return new DruidDataSource();
+    }
+}
