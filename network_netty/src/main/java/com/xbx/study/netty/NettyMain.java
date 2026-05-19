0d39258be49079113c6d7fa502c04a0e40b0579d
xing <3154401838@qq.com>
2026-05-19 22:23:27 +0800

    netty module add

---
+++ network_netty/src/main/java/com/xbx/study/netty/NettyMain.java
@@ -0,0 +1,11 @@
+package com.xbx.study.netty;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NettyMain {
+    public static void main(String[] args) {
+        SpringApplication.run(NettyMain.class,args);
+    }
+}
