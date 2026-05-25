4263d96a93d6e87ea1b01bdf4109348b9db732aa
xingbingxuan <3154401838@qq.com>
2026-05-25 14:33:26 +0800

    log update

--- cloud_web/src/main/java/com/xbx/study/web/CloudWeb.java
+++ cloud_web/src/main/java/com/xbx/study/web/CloudWeb.java
@@ -6,6 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class CloudWeb {
     public static void main(String[] args) {
-        SpringApplication.run(CloudWeb.class,args);
+        SpringApplication.run(CloudWeb.class, args);
     }
 }
