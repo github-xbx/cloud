12e73e63809089ae8f1410b722779c7071c9f4f6
xingbingxuan <3154401838@qq.com>
2026-07-09 16:59:11 +0800

    dubbo + grpc + resilirnce4j

--- cloud_dubbo_0/src/main/java/com/xbx/study/dubbo/rpc/DubboGrpcImpl.java
+++ cloud_dubbo_0/src/main/java/com/xbx/study/dubbo/rpc/DubboGrpcImpl.java
@@ -1,9 +1,12 @@
-package com.xbx.study.dubbo.service;
+package com.xbx.study.dubbo.rpc;
 
 
 import com.xbx.study.dubbo.common.grpc.DubboDubboGrpcServiceTriple.DubboGrpcServiceImplBase;
+import com.xbx.study.dubbo.common.grpc.DubboGrpcService;
 import com.xbx.study.dubbo.common.grpc.GrpcReq;
 import com.xbx.study.dubbo.common.grpc.GrpcResp;
+import io.github.resilience4j.ratelimiter.RequestNotPermitted;
+import io.github.resilience4j.ratelimiter.annotation.RateLimiter;
 import org.apache.dubbo.config.annotation.DubboService;
 
 /**
@@ -12,7 +15,7 @@ import org.apache.dubbo.config.annotation.DubboService;
  * 继承 DubboGrpcServiceImplBase（不是直接实现 DubboGrpcService 接口），
  * 基类已提供 helloAsync 等异步方法的默认实现，只需重写同步的 hello() 即可。
  */
-@DubboService(protocol = "tri")
+@DubboService(protocol = "tri", group = "group_grpc", interfaceClass = DubboGrpcService.class)
 public class DubboGrpcImpl extends DubboGrpcServiceImplBase {
 
 
